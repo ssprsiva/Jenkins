43abe665ec24071dc75a39e52d6c46d457610c6c
ssprsiva <62172018+ssprsiva@users.noreply.github.com>
2020-09-16 23:45:49 +0530

    Create shell1.sh

---
+++ shell1.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+clear
+
+city=("Hyderabad" "Bangalore" "Chennai" "Mumbai" "Delhi")
+
+
+echo "First city:" ${city[0]}
+echo "2nd city:" ${city[1]}
+echo "3rd city:" ${city[2]}
+echo "4th city:" ${city[3]}
+echo "5th city:" ${city[4]}
